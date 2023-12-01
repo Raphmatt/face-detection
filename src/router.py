285c46de7c1da8b205c3fb35824ad8d54c8f3038
Raphael <71792812+Raphmatt@users.noreply.github.com>
2023-12-01 13:31:24 +0100

    Refactor main application structure, introduce routing and services

---
+++ src/router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from fastapi import UploadFile, File
+
+router = APIRouter()
+
+@router.get("/heartbeat")
+async def heartbeat():
+    return {"status": "ok"}
+
+
+@router.post("/image/process")
+async def create_upload_file(file: UploadFile = File(...)):
+    return False;
+    #return {"item_id": image_id, "file_info": file.filename}
---
+++ src/service.py
@@ -0,0 +1,10 @@
+import cv2
+import mediapipe as mp
+
+async def recognize_face(image):
+    # Implement face recognition logic
+    pass
+
+async def process_face(image):
+    # Implement face processing logic
+    pass
