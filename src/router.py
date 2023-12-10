e5b34f714baa7497e0da200faaa2122ac01cddb1
Pascal <pascal@dummy.com>
2023-12-10 18:11:15 +0100

    add recognize_face function

--- src/router.py
+++ src/router.py
@@ -3,6 +3,7 @@ from fastapi import UploadFile, File
 
 router = APIRouter()
 
+
 @router.get("/heartbeat")
 async def heartbeat():
     return {"status": "ok"}
@@ -10,5 +11,5 @@ async def heartbeat():
 
 @router.post("/image/process")
 async def create_upload_file(file: UploadFile = File(...)):
-    return False;
-    #return {"item_id": image_id, "file_info": file.filename}
+    return False
+    # return {"item_id": image_id, "file_info": file.filename}
--- src/service.py
+++ src/service.py
@@ -1,9 +1,41 @@
 import cv2
 import mediapipe as mp
 
+
 async def recognize_face(image):
-    # Implement face recognition logic
-    pass
+    mp_face_detection = mp.solutions.face_detection
+    face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.3)
+
+    height, width, _ = image.shape
+
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    results = face_detection.process(rgb_image)
+
+    num_faces = 0
+    if results.detections:
+        num_faces = len(results.detections)
+        print(f"Number of faces found: {num_faces}")
+
+        if num_faces == 1:
+            bboxc = results.detections[0].location_data.relative_bounding_box
+            ih, iw, _ = rgb_image.shape
+            bbox = int(bboxc.xmin * iw), int(bboxc.ymin * ih), int(bboxc.width * iw), int(bboxc.height * ih)
+            print(bbox)
+            x, y, w, h = bbox
+
+            x_crop = max(0, x - 300)
+            y_crop = max(0, y - 300)
+            w_crop = min(width - x_crop, w + 600)
+            h_crop = min(height - y_crop, h + 600)
+
+            cropped_image = image[y_crop:y_crop + h_crop, x_crop:x_crop + w_crop]
+
+        else:
+            print("Found more than one face")
+    else:
+        print("No faces found in the image.")
+
 
 async def process_face(image):
     # Implement face processing logic
