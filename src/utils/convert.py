092365af4a681bcf659e25f1f4bd95366b644973
Raphael <71792812+Raphmatt@users.noreply.github.com>
2023-12-14 22:17:09 +0100

    add missing pydoc to convert.py

--- src/utils/convert.py
+++ src/utils/convert.py
@@ -4,6 +4,11 @@ import mediapipe as mp
 
 
 def convert_to_opencv_image(numpy_image: np.ndarray) -> np.ndarray:
+    """
+    Convert a numpy array to an OpenCV image
+    :param numpy_image: The numpy array to convert
+    :return: The OpenCV image
+    """
     # Convert to OpenCV image format if needed
     # (Assuming the input is already in RGB format)
     opencv_image = numpy_image.copy()
@@ -11,12 +16,22 @@ def convert_to_opencv_image(numpy_image: np.ndarray) -> np.ndarray:
 
 
 def convert_to_mp_image(numpy_image: np.ndarray) -> mp.Image:
+    """
+    Convert a numpy array to a MediaPipe image
+    :param numpy_image: The numpy array to convert
+    :return: The MediaPipe image
+    """
     # Create the MediaPipe Image from numpy array (assuming RGB format)
     mp_image = mp.Image(mp.ImageFormat.SRGB, numpy_image)
     return mp_image
 
 
 def convert_to_rgb(numpy_image: np.ndarray) -> np.ndarray:
+    """
+    Convert a numpy image to RGB format
+    :param numpy_image: The numpy image to convert
+    :return: The converted numpy image
+    """
     # If the image has 4 channels (e.g., RGBA), convert to 3 channels (RGB)
     if numpy_image.shape[-1] == 4:
         numpy_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGBA2RGB)
