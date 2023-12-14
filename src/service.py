2f01df94148819bd8598e2b682aab4529b2a68d0
Raphael <71792812+Raphmatt@users.noreply.github.com>
2023-12-14 22:10:23 +0100

    add unittests add testdata files add face detection add tflite model for face detection add convert.py utilities

--- src/service.py
+++ src/service.py
@@ -1,42 +1,70 @@
-import cv2
+import io
+import os
+
 import mediapipe as mp
+import numpy as np
+from PIL import Image
+from fastapi import UploadFile, File
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+from src.utils.convert import convert_to_rgb
 
 
-async def recognize_face(image):
-    mp_face_detection = mp.solutions.face_detection
-    face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.3)
+async def read_image_file(file: UploadFile = File(...)) -> np.ndarray:
+    """
+    Read an uploaded image file into a numpy array
 
-    height, width, _ = image.shape
+    Note: This function cannot be tested with pytest because it requires an UploadFile object
+    TODO (optional): Refactor this function to make it testable
+    :param file: The uploaded file
+    :return: The image as a numpy array
+    """
+    # Read the image file into memory
+    contents = await file.read()
 
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # Convert the contents to a PIL Image
+    pil_image = Image.open(io.BytesIO(contents))
 
-    results = face_detection.process(rgb_image)
+    # Convert the PIL image to a numpy array
+    numpy_image = np.array(pil_image)
 
-    num_faces = 0
-    if results.detections:
-        num_faces = len(results.detections)
-        print(f"Number of faces found: {num_faces}")
-
-        if num_faces == 1:
-            bboxc = results.detections[0].location_data.relative_bounding_box
-            ih, iw, _ = rgb_image.shape
-            bbox = int(bboxc.xmin * iw), int(bboxc.ymin * ih), int(bboxc.width * iw), int(bboxc.height * ih)
-            print(bbox)
-            x, y, w, h = bbox
-
-            x_crop = max(0, x - 300)
-            y_crop = max(0, y - 300)
-            w_crop = min(width - x_crop, w + 600)
-            h_crop = min(height - y_crop, h + 600)
-
-            cropped_image = image[y_crop:y_crop + h_crop, x_crop:x_crop + w_crop]
-
-        else:
-            print("Found more than one face")
-    else:
-        print("No faces found in the image.")
+    return numpy_image
 
 
-async def process_face(image):
+async def process_image(file: UploadFile = File(...)) -> np.ndarray:
+    np_image = await read_image_file(file)
+    np_image = convert_to_rgb(np_image)
+
+    if detect_face_count(np_image) == 1:
+        return np_image
+
+    return False
+    pass
+
+
+def detect_face_count(mp_image: mp.Image) -> int:
+    # Implement face recognition logic
+
+    # STEP 1: Create an FaceDetector object.
+    base_options = python.BaseOptions(
+        model_asset_path=os.path.join(
+            os.path.dirname(__file__),
+            'mp_models/face_detection/blaze_face_short_range.tflite'))
+    options = vision.FaceDetectorOptions(
+        base_options=base_options,
+        min_detection_confidence=0.7)
+    detector = vision.FaceDetector.create_from_options(options)
+
+    # STEP 2: Detect faces in the input image.
+    detection_result = detector.detect(mp_image)
+
+    # STEP 3: Count the number of faces detected.
+    face_count = len(detection_result.detections)
+
+    return face_count
+
+
+async def process_face(image: np.ndarray) -> np.ndarray:
     # Implement face processing logic
     pass
