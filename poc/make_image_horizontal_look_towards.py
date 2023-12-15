018541046e41aaffae0d4aa61cc1f5084a7fef30
Raphael <71792812+Raphmatt@users.noreply.github.com>
2023-12-15 13:38:53 +0100

    add poc files

---
+++ poc/make_image_horizontal_look_towards.py
@@ -0,0 +1,105 @@
+import time
+
+import cv2
+import mediapipe as mp
+import numpy as np
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+mp_drawing = mp.solutions.drawing_utils
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
+
+mp_face_mesh = mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
+
+BG_COLOR = (0, 255, 196)
+MODEL = "/Users/raphael/Downloads/selfie_multiclass_256x256.tflite"
+
+cap = cv2.VideoCapture(1)
+prevTime = 0
+
+# Create the options that will be used for ImageSegmenter
+base_options_segmenter = python.BaseOptions(model_asset_path=MODEL)
+options_segmenter = vision.ImageSegmenterOptions(base_options=base_options_segmenter,
+                                                 output_category_mask=True)
+
+
+def calculate_face_transformation(face_landmarks, image_shape):
+    # Select key landmarks for the transformation
+    # For example: corners of the eyes, nose tip, and chin
+    # Replace indices with the correct ones for your model
+    landmarks_indices = [33, 133, 0, 152]  # Example indices
+
+    # Destination points - where we want the landmarks to be after transformation
+    # These would be the points for a front-facing face
+    dst_points = np.float32([
+        [100, 100],  # Corresponding point for landmark 33
+        [200, 100],  # Corresponding point for landmark 133
+        # ... add more corresponding points ...
+    ])
+
+    # Extract the corresponding source points from the face landmarks
+    src_points = np.float32([
+        [face_landmarks.landmark[i].x * image_shape[1], face_landmarks.landmark[i].y * image_shape[0]]
+        for i in landmarks_indices
+    ])
+
+    # Calculate the transformation matrix
+    transformation_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+
+    return transformation_matrix
+
+
+with mp_face_mesh.FaceMesh(
+        static_image_mode=False,
+        max_num_faces=1,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as face_mesh:
+    with python.vision.ImageSegmenter.create_from_options(options_segmenter) as segmenter:
+        bg_image = None
+
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                continue
+
+            # Convert the BGR image to RGB (if your model expects RGB input)
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            # Create the MediaPipe Image from numpy array (assuming RGB format)
+            mp_image = mp.Image(mp.ImageFormat.SRGB, image_rgb)
+
+            # segmentation_result = segmenter.segment(mp_image)
+            # category_mask = segmentation_result.category_mask
+
+            face_mesh_results = face_mesh.process(image)
+
+            output_image = image_rgb
+
+            if face_mesh_results.multi_face_landmarks:
+                for face_landmarks in face_mesh_results.multi_face_landmarks:
+                    transformation_matrix = calculate_face_transformation(face_landmarks, image.shape)
+
+                    # Perform the transformation
+                    output_image = cv2.warpPerspective(output_image, transformation_matrix,
+                                                       (image.shape[1], image.shape[0]))
+
+                    # Draw landmarks after transformation
+                    mp_drawing.draw_landmarks(
+                        image=output_image,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    )
+
+            # Convert the RGB image back to BGR
+            output_image_bgr = cv2.cvtColor(output_image, cv2.COLOR_RGB2BGR)
+
+            currTime = time.time()
+            fps = 1 / (currTime - prevTime)
+            prevTime = currTime
+            cv2.putText(output_image_bgr, f'fps: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 2)
+
+            cv2.imshow('DIY Background removal', output_image_bgr)
+            if cv2.waitKey(5) & 0xFF == 27:  # Exit on pressing ESC
+                break
---
+++ poc/remove_background_segment_cam.py
@@ -0,0 +1,75 @@
+import time
+
+import cv2
+import mediapipe as mp
+import numpy as np
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+mp_drawing = mp.solutions.drawing_utils
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
+
+BG_COLOR = (0, 255, 196)
+MODEL = "/Users/raphael/Downloads/selfie_multiclass_256x256.tflite"
+
+
+cap = cv2.VideoCapture(1)
+prevTime = 0
+
+# Create the options that will be used for ImageSegmenter
+base_options = python.BaseOptions(model_asset_path=MODEL)
+options = vision.ImageSegmenterOptions(base_options=base_options,
+                                       output_category_mask=True)
+
+with python.vision.ImageSegmenter.create_from_options(options) as segmenter:
+    bg_image = None
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+
+        # Convert the BGR image to RGB (if your model expects RGB input)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Create the MediaPipe Image from numpy array (assuming RGB format)
+        mp_image = mp.Image(mp.ImageFormat.SRGB, image_rgb)
+
+        segmentation_result = segmenter.segment(mp_image)
+        category_mask = segmentation_result.category_mask
+
+        # Select only the hair category (usually represented by index 1)
+        hair_condition = (category_mask.numpy_view() == 1)
+        body_condition = (category_mask.numpy_view() == 2)
+        face_skin_condition = (category_mask.numpy_view() == 3)
+        clothes_condition = (category_mask.numpy_view() == 4)
+
+        # Combine the conditions (logical OR)
+        combined_condition = np.logical_or(hair_condition, body_condition)
+        combined_condition = np.logical_or(combined_condition, face_skin_condition)
+        combined_condition = np.logical_or(combined_condition, clothes_condition)
+
+        # Stack the combined condition to apply it on RGB channels
+        combined_condition_stacked = np.stack((combined_condition,) * 3, axis=-1)
+
+        # Create a black background image
+        black_background = np.zeros_like(image_rgb)
+
+        # create a white background image
+        white_background = np.ones_like(image_rgb) * 255
+
+        # Apply the mask: keep only the hair, make everything else black
+        output_image = np.where(combined_condition_stacked, image_rgb, white_background)
+
+        # Convert the RGB image back to BGR
+        output_image_bgr = cv2.cvtColor(output_image, cv2.COLOR_RGB2BGR)
+
+        currTime = time.time()
+        fps = 1 / (currTime - prevTime)
+        prevTime = currTime
+        cv2.putText(output_image_bgr, f'fps: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 2)
+
+        cv2.imshow('DIY Background removal', output_image_bgr)
+        if cv2.waitKey(5) & 0xFF == 27:  # Exit on pressing ESC
+            break
